29ac21ded2d0f90ea7f8ddc7a282f70b2adeae4e
Ashutosh Apurva <149580171+AshutoshA14@users.noreply.github.com>
2025-09-05 07:52:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+// Simple Node.js script using lodash
+
+const _ = require("lodash");
+
+const numbers = [10, 20, 30, 40, 50];
+
+// Calculate average using lodash
+const avg = _.mean(numbers);
+
+console.log("Numbers:", numbers);
+console.log("Average:", avg);
